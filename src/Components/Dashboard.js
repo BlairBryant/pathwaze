b76834758f97e8458f83da3b2e79b11e79aead0c
Blair <blairslc@gmail.com>
2018-05-01 18:31:51 -0600

    header semi-styled

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -12,15 +12,30 @@ class Dashboard extends Component {
                 <header className='row'>
                     <section className='row hamAndLogo'>
                         <div>=</div>
-                        <p>pathwaze</p>
+                        <p>PATHWAZE</p>
                     </section>
                     <section>
-
+                        <div className='companiesContainer'>
+                            <div className='row companiesDiv'>
+                                <h3>Artemis</h3>
+                                <h3>+</h3>
+                                <h3>Portico</h3>
+                            </div>
+                            <h2>Partnership Plan</h2>
+                        </div>
                     </section>
                     <section>
-
+                        <div className='row ctr'>
+                        <div className='row ctr userImg'>E</div>
+                        <h4>John Gray</h4>
+                        <h4>\/</h4>
+                        </div>
                     </section>
                 </header>
+                <section>
+                    <div className='row aln top'></div>
+                </section>
+
             </div>
         )
     }
